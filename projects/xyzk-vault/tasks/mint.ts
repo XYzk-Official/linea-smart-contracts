ff80880862434df1befca155d3289c0ba90c9ec3
JasonStaham <jason_staham_20@proton.me>
2023-12-25 10:16:05 +0700

    chore: Fix mint token task

--- projects/xyzk-vault/tasks/mint.ts
+++ projects/xyzk-vault/tasks/mint.ts
@@ -1,9 +1,10 @@
 import { task } from "hardhat/config";
+import { ethers } from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 task("mint", "Mint token to specific address")
   .addParam("address", "Receiver address")
-  .addParam("amount", "Amount")
+  .addParam("amount", "Amount in ethers")
   .setAction(async (taskArgs: any, hre: HardhatRuntimeEnvironment) => {
     const address = (taskArgs.address || "").trim();
     const amount = (taskArgs.amount || "").trim();
@@ -12,11 +13,11 @@ task("mint", "Mint token to specific address")
     if (isAddress) {
       try {
         const contract = await hre.ethers.getContractAt(
-          "BeraSleepToken",
-          "0xC938173CccA0f3C917A0dC799B3dbEF89626fE2B",
+          "XYzKToken",
+          "0x707dc1041dc702a83F7a0af57dDe1a3cdAC9643e",
           account[0]
         );
-        const result = await contract.connect(account[0]).mint(address, amount);
+        const result = await contract.connect(account[0]).mint(address, ethers.utils.parseEther(amount));
         console.log("Transaction done with txh = ", result);
       } catch (error) {
         console.log(error);
