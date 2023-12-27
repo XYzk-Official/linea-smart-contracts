dfa5d7e0a76c7164fdb099069ab16101937dff9f
JasonStaham <jason_staham_20@proton.me>
2023-12-27 15:31:15 +0700

    chore: Update ifo initializable contract

--- projects/ifo/scripts/3-deploy-ifo-v2.ts
+++ projects/ifo/scripts/3-deploy-ifo-v2.ts
@@ -1,4 +1,5 @@
 import { ethers, network, run } from "hardhat";
+import { parseEther } from "ethers/lib/utils";
 import config from "../config";
 
 const main = async () => {
@@ -7,18 +8,46 @@ const main = async () => {
   const admin = deployer.address;
 
   console.log(`Deploying to: ${name}... with admin address`, admin);
-  const IFOV2 = await ethers.getContractFactory("IFOV2");
+  const IFOV2Contract = await ethers.getContractFactory("IFOV2");
+  const lpToken = "0x37e16520C636E184E766A13f0339F61f263a56a5";
+  const offeringToken = "0x04c8b978C739dfEc62a2F913c870cf47C30228EC";
+  const profile = "0x0E59D921E2E8E14AB20276b064abAB0417c30A57";
+  const startBlock = "1703749773"; // Thursday, December 28, 2023 2:49:33 PM
+  const endBlock = "1703922573"; // Saturday, December 30, 2023 2:49:33 PM
 
-  const ifoV2 = await IFOV2.deploy(
-    "0xcBa39955e07CC142ABCbDA272c9C9faF0C3e00A9",
-    "0x4ED0b454985F847AE04B31443057011a65F2569e",
-    "0x0E59D921E2E8E14AB20276b064abAB0417c30A57",
-    "0",
-    "0",
-    admin
+  const offeringAmountPool0 = parseEther("50");
+  const raisingAmountPool0 = parseEther("5");
+
+  // IFO Pool 1
+  const offeringAmountPool1 = parseEther("1000");
+  const raisingAmountPool1 = parseEther("100");
+  const limitPerUserInLp = parseEther("0.5");
+
+  const campaignId = "512100001";
+  const numberPoints = "100";
+  const thresholdPoints = parseEther("0.035");
+
+  const ifoV2 = await IFOV2Contract.deploy(lpToken, offeringToken, profile, startBlock, endBlock, admin);
+  await ifoV2.deployed();
+
+  console.log("New IFOV2 deployed to:", ifoV2.address);
+  await ifoV2.setPool(
+    offeringAmountPool0,
+    raisingAmountPool0,
+    limitPerUserInLp,
+    false, // tax
+    "0"
   );
 
-  console.log("IFOV2 deployed to:", ifoV2.address);
+  await ifoV2.setPool(
+    offeringAmountPool1,
+    raisingAmountPool1,
+    "0",
+    true, // tax
+    "1"
+  );
+
+  await ifoV2.updatePointParameters(campaignId, numberPoints, thresholdPoints);
 };
 
 main()
--- projects/ifo/scripts/5-setup-ifo-initializable.ts
+++ projects/ifo/scripts/5-setup-ifo-initializable.ts
@@ -5,12 +5,12 @@ const main = async () => {
   const [deployer] = await ethers.getSigners();
   const admin = deployer.address;
 
-  const IFOInitializableAddress = "0x46dB1b64C668880c55251cC7924247605dD6B09A";
-  const lpToken = "0xcBa39955e07CC142ABCbDA272c9C9faF0C3e00A9";
-  const offerToken = "0x4ED0b454985F847AE04B31443057011a65F2569e";
+  const IFOInitializableAddress = "0x1b79Ab6F3c4c80780f61614eBEaC7B2493A6e051";
+  const lpToken = "0x37e16520C636E184E766A13f0339F61f263a56a5";
+  const offerToken = "0x04c8b978C739dfEc62a2F913c870cf47C30228EC";
   const profile = "0x0E59D921E2E8E14AB20276b064abAB0417c30A57";
-  const startBlock = "0";
-  const endBlock = "0";
+  const startBlock = "1703749773";
+  const endBlock = "1703922573";
   const maxBufferBlocks = "400000";
 
   const contract = await hre.ethers.getContractAt("IFOInitializable", IFOInitializableAddress, deployer);
